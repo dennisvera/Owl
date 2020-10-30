d7c80d52a47adb98bfd264d627f4a3cb98ef822b
DennisVera <dennisvera.z@gmail.com>
2020-10-30 14:22:22 -0400

    Initalize project

---
+++ Owl/SceneDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  SceneDelegate.swift
+//  Owl
+//
+//  Created by Dennis Vera on 10/30/20.
+//  Copyright © 2020 Dennis Vera. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  // MARK: - Properties
+
+  var window: UIWindow?
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    // Initialize Window
+    guard let windowScene = scene as? UIWindowScene else { return }
+    window = UIWindow(windowScene: windowScene)
+
+    // Configure Window
+    window?.rootViewController = ViewController()
+
+    // Make Key and Visible
+    window?.makeKeyAndVisible()
+  }
+
+  func sceneDidDisconnect(_ scene: UIScene) {}
+
+  func sceneDidBecomeActive(_ scene: UIScene) {}
+
+  func sceneWillResignActive(_ scene: UIScene) {}
+
+  func sceneWillEnterForeground(_ scene: UIScene) {}
+
+  func sceneDidEnterBackground(_ scene: UIScene) {}
+}
---
+++ Owl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Owl
+//
+//  Created by Dennis Vera on 10/30/20.
+//  Copyright © 2020 Dennis Vera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+
+    view.backgroundColor = .yellow
+  }
+}
