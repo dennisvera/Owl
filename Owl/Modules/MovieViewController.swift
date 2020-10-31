b1fe9aff495d812e1279f21b1b096c2b88db7631
DennisVera <dennisvera.z@gmail.com>
2020-10-31 00:07:49 -0400

    Implement Collecton View Compositional Layout

--- Owl/Modules/MovieViewController.swift
+++ Owl/Modules/MovieViewController.swift
@@ -12,37 +12,60 @@ final class MovieViewController: UIViewController {
 
   // MARK: - Properties
 
-  private weak var collectionView: UICollectionView!
+  private var collectionView: UICollectionView!
 
   // MARK: - View Life Cycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    view.backgroundColor = .white
-
     fetchData()
     setupCollectionView()
   }
 
   private func setupCollectionView() {
-    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    // Configure Collection View
+    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createCollectionViewLayout())
     collectionView.delegate = self
     collectionView.dataSource = self
+    collectionView.backgroundColor = .white
     collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     collectionView.translatesAutoresizingMaskIntoConstraints = false
 
+    // Constraints
     let constraints = [
-      collectionView.topAnchor.constraint(equalTo: view.topAnchor),
-      collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
-      collectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
-      collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+      collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+      collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+      collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
     ]
 
     view.addSubview(collectionView)
     NSLayoutConstraint.activate(constraints)
+  }
 
-    self.collectionView = collectionView
+  private func createCollectionViewLayout() -> UICollectionViewLayout {
+    // Define Item Size
+    let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(200.0))
+    // Create Item
+    let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+    // Define Group Size
+    let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(200.0))
+    // Create Group
+    let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+    // Create Section
+    let section = NSCollectionLayoutSection(group: group)
+    // Configure Section
+    section.contentInsets = NSDirectionalEdgeInsets(top: 0.0, leading: 20.0, bottom: 0.0, trailing: 20.0)
+     // group spacing
+    section.interGroupSpacing = 15
+
+    // Create Layout
+    let layout = UICollectionViewCompositionalLayout(section: section)
+
+    return layout
   }
 
   private func fetchData() {
